68175c889ea44442c4d9afa168732ff1371255d7
miha14011988 <smmcopy@ya.ru>
2022-04-13 16:20:47 +0300

    Arrays

--- src/ArraysOperations.java
+++ src/ArraysOperations.java
@@ -1,6 +1,155 @@
+import java.util.Arrays;
 
 public class ArraysOperations {
+    public boolean isEven(int[] array, int index){
+        if(array == null || array.length ==0){
+            throw new NullPointerException("Array is empty");
+        }
+        if(index <0 || index >= array.length){
+            throw new ArrayIndexOutOfBoundsException("index is incorrect");
+        }
+        return array[index] % 2 == 0;
+    }
+
+    public int sumElement(int[] array){
+        int sum =0;
+        if(array != null && array.length !=0){
+            for(int i :array){
+                sum = sum+i;
+            }
+        }
+        return sum;
+    }
+
+    public int countElement(int[] array){
+        if(array == null || array.length ==0){
+            throw new NullPointerException("Array is empty");
+        }
+        return array.length;
+    }
+
+    public String minimum(int[] array){
+        String s;
+        int index =0;
+        if(array == null || array.length ==0){
+            throw new NullPointerException("Array is empty");
+
+        }else {
+            int minimum = array[0];
+            for(int i =0; i < array.length ;i++){
+                if(array[i] < minimum){
+                    minimum =array[i];
+                    index = i;
+                }
+            }
+            s = "minimum: array["+index+"] = "+minimum;
+        }
+        return s;
+    }
+
+    public String maximum(int[] array){
+        String s;
+        int index =0;
+        if(array == null || array.length ==0){
+            throw new NullPointerException("Array is empty");
+
+        }else {
+            int maximum = array[0];
+            for(int i =0; i < array.length ;i++){
+                if(array[i] > maximum){
+                    maximum =array[i];
+                    index = i;
+                }
+            }
+            s = "maximum: array["+index+"] = "+maximum;
+        }
+        return s;
+    }
+
+    public String getValue(int[] array,int value){
+        String s =null;
+        int number=0;
+        int index =0;
+        if(array == null || array.length ==0){
+            throw new NullPointerException("Array is empty");
+        }
+        if(value >= array.length || value < 0){
+            throw new ArrayIndexOutOfBoundsException("index is incorrect");
+        }
+        for(int i=0; i < array.length ;i++){
+            if(array[i] == value){
+                number =value;
+                index = i;
+            }
+            s = "value: array["+index+"] = "+number;
+        }
+        return s;
+    }
+
+    public String replaceValue(int[] array, int value , int replace){
+        int index =0;
+        int number =0;
+        if(array == null || array.length ==0){
+            throw new NullPointerException("Array is empty");
+        }
+        if(value >= array.length || value < 0){
+            throw new ArrayIndexOutOfBoundsException("index is incorrect");
+        }
+        if(replace <= 0){
+            throw new IllegalArgumentException("number is negative");
+        }
+
+        for(int i =0; i < array.length ;i++){
+            if(array[i] == value){
+                array[i] =replace;
+                number =array[i];
+                index = i;
+            }
+        }
+        return "replace: array["+index+"] = "+number;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        ArraysOperations a = new ArraysOperations();
+        int[] array = new int[10];
+        int[] reverseArray = new int[10];
+        System.out.println("array: "+ Arrays.toString(array));
+        int number =1;
+
+        for(int i =0; i < array.length ;i++){
+            array[i] = number;
+            number++;
+        }
+
+        System.out.println("array: "+ Arrays.toString(array));
+
+        for(int i = reverseArray.length-1; i >=0; i--){
+            reverseArray[i] = number;
+            number++;
+        }
+
+        System.out.println("reverse array: "+ Arrays.toString(reverseArray));
+        boolean result1 = a.isEven(array,0);
+        boolean result2 = a.isEven(array,1);
+        System.out.println("a.isEven(array,0) = "+result1);
+        System.out.println("a.isEven(array,1) = "+result2);
+        int result3 = a.sumElement(array);
+        System.out.println("sum = "+result3);
+        int result4= a.countElement(array);
+        System.out.println("count element = "+result4);
+        String result5 = a.minimum(array);
+        String result6 = a.minimum(reverseArray);
+        System.out.println(result5);
+        System.out.println(result6);
+        String result7 = a.maximum(array);
+        String result8 = a.maximum(reverseArray);
+        System.out.println(result7);
+        System.out.println(result8);
+        String result9 = a.getValue(array,7);
+        System.out.println(result9);
+        String result10 = a.replaceValue(array,7,1000);
+        System.out.println(result10);
+        System.out.println("array: "+Arrays.toString(array));
+
     }
 }
--- src/Sorting.java
+++ src/Sorting.java
@@ -1,2 +1,93 @@
+import java.util.Arrays;
+
 public class Sorting {
+    private int a;
+    private static int b;
+
+    @FunctionalInterface
+    private interface Select{
+        int[] selectionSort(int[] array);
+    }
+
+    public Select select(){
+        return array ->  {
+            {
+                if(array == null || array.length ==0){
+                    throw new ArrayIndexOutOfBoundsException("Array is empty");
+                }else {
+                    for(int i =0; i < array.length ;i++){
+                        int min = array[i];
+                        int minIndex = i;
+                        for(int j = i+1; j < array.length ; j++){
+                            if(array[j] < min){
+                                min = array[j];
+                                minIndex = j;
+                            }
+                        }
+                        int temporary = array[i];
+                        array[i] = min;
+                        array[minIndex] =temporary;
+                    }
+                }
+                return array;
+            }
+
+        };
+    }
+
+
+
+
+    public static class Insert{
+
+        public int[] insertSort(int[] array){
+            if(array == null || array.length ==0){
+                throw new ArrayIndexOutOfBoundsException("Array is empty");
+            }else {
+                for(int i = 1; i <array.length ;i++){
+                    int current = array[i];
+                    int j = i-1;
+                    while (j>=0 && array[j] > current){
+                        array[j+1] =array[j];
+                        j--;
+                    }
+                    array[j+1] = current;
+                }
+            }
+            return array;
+        }
+    }
+
+    private class Bubble{
+
+        public int[] bubbleSort(int[] array){
+            if(array == null || array.length ==0){
+                throw new ArrayIndexOutOfBoundsException("Array is empty");
+            }else {
+                for(int i =0; i <array.length-1  ;i++){
+                    int temporary;
+                    if(array[i] > array[i+1]){
+                        temporary = array[i];
+                        array[i] = array[i+1];
+                        array[i+1]= temporary;
+                    }
+                }
+            }
+            return array;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {90,12,34,27,78};
+        System.out.println("array: "+ Arrays.toString(array));
+        Sorting sorting = new Sorting();
+        Bubble bubble = sorting.new Bubble();
+        int[] sortedArray1 = bubble.bubbleSort(array);
+        System.out.println("bubble sorting: "+Arrays.toString(sortedArray1));
+        Sorting.Insert insert = new Insert();
+        int[] sortedArray2 = insert.insertSort(array);
+        System.out.println("insert sorting: "+Arrays.toString(sortedArray2));
+        int[] sortedArray3 = sorting.select().selectionSort(array);
+        System.out.println("select sorting: "+Arrays.toString(sortedArray3));
+    }
 }
